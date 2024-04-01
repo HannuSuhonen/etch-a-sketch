2b6575db627d23e7560091d81cd031f0ee629e7a
hannu <49480722+HannuSuhonen@users.noreply.github.com>
2024-04-01 18:50:04 +0300

    Cuztomize grid size with slider

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const gridContainer = document.querySelector(".grid-container");
+const slider = document.querySelector("#myRange");
 
 let mouseEntered = false;
 let mouseClicked = false;
 
 function createGrid(iterations = 36){
-    for(let i = 0; i < iterations; i++){
+
+    deleteGrid(gridContainer);
+    let roundNumber = roundToNearestPerfectSquare(iterations);
+
+    for(let i = 0; i < roundNumber; i++){
         const gridItem = document.createElement("div");
         gridItem.classList.add("grid-item");
         
@@ -24,7 +29,33 @@ function createGrid(iterations = 36){
             item.classList.add("change-color")
         })
     });
+
+    let columns = Math.sqrt(roundNumber);
+    // Set the CSS variable to adjust the number of columns dynamically
+    document.documentElement.style.setProperty("--columns", columns);
 }
 
+function roundToNearestPerfectSquare(number) {
+    // Find the square root of the number
+    const squareRoot = Math.sqrt(number);
+    
+    // Round the square root to the nearest integer
+    const roundedSquareRoot = Math.round(squareRoot);
+    
+    // Calculate the nearest perfect square
+    const nearestPerfectSquare = Math.pow(roundedSquareRoot, 2);
+    
+    return nearestPerfectSquare;
+}
 
-createGrid();
+function deleteGrid(parent){
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+slider.oninput = function() {
+    createGrid(this.value);
+}
+
+createGrid(500);
