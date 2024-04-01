5bf5f8ac4ea66f716988826ff2bae68bca4501ba
hannu <49480722+HannuSuhonen@users.noreply.github.com>
2024-04-01 16:01:13 +0300

    Click and drag to draw

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const gridContainer = document.querySelector(".grid-container");
 
+let mouseEntered = false;
+let mouseClicked = false;
+
 function createGrid(iterations = 36){
     for(let i = 0; i < iterations; i++){
         const gridItem = document.createElement("div");
@@ -10,11 +13,18 @@ function createGrid(iterations = 36){
 
     const gridItems = document.querySelectorAll(".grid-item");
 
+
     gridItems.forEach(item => {
-        item.addEventListener("click" ,() => {
-            item.classList.length <= 1 ? item.classList.add("change-color") : item.classList.remove("change-color")
+        item.addEventListener("mouseenter" ,(event) => {
+            if(event.buttons == 1 || event.buttons == 3){
+                item.classList.add("change-color")
+            }
+        })
+        item.addEventListener("mousedown" ,() => {
+            item.classList.add("change-color")
         })
     });
 }
+
 
 createGrid();
